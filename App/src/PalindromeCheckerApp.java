8ff579899299bd2d304486c286307407b54753a1
Shrey-Arc <shreykumarsks@gmail.com>
2026-02-19 10:35:06 +0530

    Array based palindrome checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,22 +1,24 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String original = "Madam";
+        String input = "Racecar";
 
-        String reversed = "";
+        char[] charArray = input.toLowerCase().toCharArray();
 
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+        int left = 0;
+        int right = charArray.length - 1;
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (charArray[left] != charArray[right]) {
+                isPalindrome = false;
+                break;
+            }
+
+            left++;
+            right--;
         }
 
-        boolean isPalindrome = original.equalsIgnoreCase(reversed);
-
-        System.out.println("Original String: " + original);
-        System.out.println("Reversed String: " + reversed);
-
-        if (isPalindrome) {
-            System.out.println("Result: The string IS a palindrome.");
-        } else {
-            System.out.println("Result: The string IS NOT a palindrome.");
-        }
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome? " + isPalindrome);
     }
 }
